146eea208d7d1aeb40fd359d8f82680743b50d8d
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-07-16 22:44:37 -0500

    fixed duplicate

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -74,16 +74,7 @@ public class MySQLUsersDao implements Users {
             rs.getString("password")
         );
     }
-  public User findByUserID(long id) {
-    String query = "SELECT * FROM users WHERE id = ? LIMIT 1";
-    try {
-      PreparedStatement stmt = connection.prepareStatement(query);
-      stmt.setLong(1, id);
-      return extractUser(stmt.executeQuery());
-    } catch (SQLException e) {
-      throw new RuntimeException("Error finding a user by username", e);
-    }
-  }
+
     public void updateUserPassword (long id, String hashedPassword) {
         String updateSql = "UPDATE users set password = ? where id = ?";
 //    We are going to replace the ? with the ID that wants to be deleted.
